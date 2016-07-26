79926368a8eed24f310b22ebc7efc21c71f10c2c
Sakshi Agarwal <SAKSHIA@microsoft.com>
2016-07-26 16:30:27 +0530

    Added complete solution for CRUD operations in DocumentDB

---
+++ DocumentDbGettingStarted/DocumentDbGettingStarted/Program.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace DocumentDbGettingStarted
+{
+    class Program
+    {
+        private const string EndpointURL = "https://sakshi2710.documents.azure.com:443/";
+        private const string PrimaryKEY = "1e5E9kOLYcUYIDOvbX6PHfCTObGVhQqPqxg4lmx6QgSo56iidvr0N6YHfzDo8QQtwPPPcRJWe7piacii7ujLZg==";
+        private DocumentClient client;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program pgm = new Program();
+                pgm.GetStartedDemo().Wait();
+            }
+            catch (DocumentClientException e)
+            {
+                Exception baseexpection = e.GetBaseException();
+                Console.WriteLine("{0} error occured: {1} , Message : {2}",e.StatusCode, e.Message, baseexpection.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        private async Task GetStartedDemo()
+        {
+            this.client = new DocumentClient(new Uri(EndpointURL), PrimaryKEY);
+            await this.CreateDatabaseIfNotExists("FamilyDB");
+            await this.CreateDocumentCollectionIfNotExists("FamilyDB", "FamilyCollection");
+            // ADD THIS PART TO YOUR CODE
+            Family andersenFamily = new Family
+            {
+                Id = "Andersen.1",
+                LastName = "Andersen",
+                Parents = new Parent[]
+                    {
+                new Parent { FirstName = "Thomas" },
+                new Parent { FirstName = "Mary Kay" }
+                    },
+                Children = new Child[]
+                    {
+                new Child
+                {
+                        FirstName = "Henriette Thaulow",
+                        Gender = "female",
+                        Grade = 5,
+                        Pets = new Pet[]
+                        {
+                                new Pet { GivenName = "Fluffy" }
+                        }
+                }
+                    },
+                Address = new Address { State = "WA", County = "King", City = "Seattle" },
+                IsRegistered = true
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB", "FamilyCollection", andersenFamily);
+
+            Family wakefieldFamily = new Family
+            {
+                Id = "Wakefield.7",
+                LastName = "Wakefield",
+                Parents = new Parent[]
+        {
+                new Parent { FamilyName = "Wakefield", FirstName = "Robin" },
+                new Parent { FamilyName = "Miller", FirstName = "Ben" }
+        },
+                Children = new Child[]
+        {
+                new Child
+                {
+                        FamilyName = "Merriam",
+                        FirstName = "Jesse",
+                        Gender = "female",
+                        Grade = 8,
+                        Pets = new Pet[]
+                        {
+                                new Pet { GivenName = "Goofy" },
+                                new Pet { GivenName = "Shadow" }
+                        }
+                },
+                new Child
+                {
+                        FamilyName = "Miller",
+                        FirstName = "Lisa",
+                        Gender = "female",
+                        Grade = 1
+                }
+        },
+                Address = new Address { State = "NY", County = "Manhattan", City = "NY" },
+                IsRegistered = false
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB", "FamilyCollection", wakefieldFamily);
+            
+            this.ExecuteSimpleQuery("FamilyDB", "FamilyCollection");
+
+            //Update
+            andersenFamily.Children[0].Grade = 6;
+
+            await this.ReplaceFamilyDocument("FamilyDB", "FamilyCollection", "Andersen.1", andersenFamily);
+
+            this.ExecuteSimpleQuery("FamilyDB", "FamilyCollection");
+
+            //Delete
+            await this.DeleteFamilyDocument("FamilyDB", "FamilyCollection", "Andersen.1");
+
+            //Delete database
+            await this.client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri("FamilyDB"));
+
+
+        }
+
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        private async Task CreateDatabaseIfNotExists(string databaseName)
+        {
+            // Check to verify a database with the id=FamilyDB does not exist
+            try
+            {
+                await this.client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(databaseName));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", databaseName);
+            }
+            catch (DocumentClientException de)
+            {
+                // If the database does not exist, create a new database
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDatabaseAsync(new Database { Id = databaseName });
+                    this.WriteToConsoleAndPromptToContinue("Created {0}", databaseName);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task CreateDocumentCollectionIfNotExists(string databaseName, string collectionName)
+        {
+            try
+            {
+                await this.client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", collectionName);
+            }
+            catch (DocumentClientException de)
+            {
+                // If the document collection does not exist, create a new collection
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    DocumentCollection collectionInfo = new DocumentCollection();
+                    collectionInfo.Id = collectionName;
+
+                    // Configure collections for maximum query flexibility including string range queries.
+                    collectionInfo.IndexingPolicy = new IndexingPolicy(new RangeIndex(DataType.String) { Precision = -1 });
+
+                    // Here we create a collection with 400 RU/s.
+                    await this.client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(databaseName),
+                        collectionInfo,
+                        new RequestOptions { OfferThroughput = 400 });
+
+                    this.WriteToConsoleAndPromptToContinue("Created {0}", collectionName);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task CreateFamilyDocumentIfNotExists(string databaseName, string collectionName, Family family)
+        {
+            try
+            {
+                await this.client.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, family.Id));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", family.Id);
+            }
+            catch (DocumentClientException de)
+            {
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), family);
+                    this.WriteToConsoleAndPromptToContinue("Created Family {0}", family.Id);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void ExecuteSimpleQuery(string databaseName, string collectionName)
+        {
+            // Set some common query options
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            // Here we find the Andersen family via its LastName
+            IQueryable<Family> familyQuery = this.client.CreateDocumentQuery<Family>(
+                    UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), queryOptions)
+                    .Where(f => f.LastName == "Andersen");
+
+            // The query is executed synchronously here, but can also be executed asynchronously via the IDocumentQuery<T> interface
+            Console.WriteLine("Running LINQ query...");
+            foreach (Family family in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", family);
+            }
+
+            // Now execute the same query via direct SQL
+            IQueryable<Family> familyQueryInSql = this.client.CreateDocumentQuery<Family>(
+                    UriFactory.CreateDocumentCollectionUri(databaseName, collectionName),
+                    "SELECT * FROM Family WHERE Family.lastName = 'Andersen'",
+                    queryOptions);
+
+            Console.WriteLine("Running direct SQL query...");
+            foreach (Family family in familyQueryInSql)
+            {
+                Console.WriteLine("\tRead {0}", family);
+            }
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        private async Task ReplaceFamilyDocument(string databaseName, string collectionName, string familyName, Family updatedFamily)
+        {
+            try
+            {
+                await this.client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, familyName), updatedFamily);
+                this.WriteToConsoleAndPromptToContinue("Replaced Family {0}", familyName);
+            }
+            catch (DocumentClientException de)
+            {
+                throw;
+            }
+        }
+
+        private async Task DeleteFamilyDocument(string databaseName, string collectionName, string documentName)
+        {
+            try
+            {
+                await this.client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, documentName));
+                Console.WriteLine("Deleted Family {0}", documentName);
+            }
+            catch (DocumentClientException de)
+            {
+                throw;
+            }
+        }
+
+        public class Family
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+            public string LastName { get; set; }
+            public Parent[] Parents { get; set; }
+            public Child[] Children { get; set; }
+            public Address Address { get; set; }
+            public bool IsRegistered { get; set; }
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+
+        public class Parent
+        {
+            public string FamilyName { get; set; }
+            public string FirstName { get; set; }
+        }
+
+        public class Child
+        {
+            public string FamilyName { get; set; }
+            public string FirstName { get; set; }
+            public string Gender { get; set; }
+            public int Grade { get; set; }
+            public Pet[] Pets { get; set; }
+        }
+
+        public class Pet
+        {
+            public string GivenName { get; set; }
+        }
+
+        public class Address
+        {
+            public string State { get; set; }
+            public string County { get; set; }
+            public string City { get; set; }
+        }
+    }
+}
---
+++ DocumentDbGettingStarted/DocumentDbGettingStarted/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DocumentDbGettingStarted")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DocumentDbGettingStarted")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a31993bb-1f0f-44fb-b908-88dd8b3e58af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
